c41f264220701da55487a4236ef8fdf75344b7e6
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-31 08:21:41 +0100

    ignore test

--- discovery-client/src/test/groovy/io/micronaut/discovery/consul/MockConfigurationDiscoverySpec.groovy
+++ discovery-client/src/test/groovy/io/micronaut/discovery/consul/MockConfigurationDiscoverySpec.groovy
@@ -22,6 +22,7 @@ import io.micronaut.discovery.consul.client.v1.ConsulClient
 import io.micronaut.runtime.server.EmbeddedServer
 import reactor.core.publisher.Flux
 import spock.lang.AutoCleanup
+import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.util.environment.RestoreSystemProperties
@@ -50,6 +51,7 @@ class MockConfigurationDiscoverySpec extends Specification {
         System.setProperty(Environment.BOOTSTRAP_CONTEXT_PROPERTY, "true")
     }
 
+    @Ignore
     void 'test read application configuration from Consul'() {
         given:
         System.setProperty("some.consul.value", "other") // consul should override
