02c750d806464269247bc90e62d1d9c10c8f73fd
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-31 09:46:57 +0100

    add to avoid failure

--- test-suite-consul-watch/build.gradle.kts
+++ test-suite-consul-watch/build.gradle.kts
@@ -9,6 +9,14 @@ dependencies {
     implementation(projects.micronautDiscoveryClient)
     implementation(mnTest.micronaut.test.junit5)
     implementation(mnTest.assertj.core)
+
+    implementation(mn.micronaut.jackson.databind)
+    /**
+     * without this dependency, the following error is observed:
+    Caused by: java.lang.NoClassDefFoundError: io/micronaut/jackson/env/JsonPropertySourceLoader
+     *
+     */
+
     implementation(mnSerde.micronaut.serde.jackson)
     implementation(platform(mnTest.boms.testcontainers))
     implementation(libs.testcontainers.junit.jupiter)
