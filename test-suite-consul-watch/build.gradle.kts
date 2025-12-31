741b0e23560529f658acb39ddae0ee4c2d27b06b
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-31 13:02:49 +0100

    remove micronaut-jackson-databind
    without this dependency, the following error is observed:
    
    ```
        Caused by: java.lang.NoClassDefFoundError: io/micronaut/jackson/env/JsonPropertySourceLoader
    ```

--- test-suite-consul-watch/build.gradle.kts
+++ test-suite-consul-watch/build.gradle.kts
@@ -9,14 +9,6 @@ dependencies {
     implementation(projects.micronautDiscoveryClient)
     implementation(mnTest.micronaut.test.junit5)
     implementation(mnTest.assertj.core)
-
-    implementation(mn.micronaut.jackson.databind)
-    /**
-     * without this dependency, the following error is observed:
-    Caused by: java.lang.NoClassDefFoundError: io/micronaut/jackson/env/JsonPropertySourceLoader
-     *
-     */
-
     implementation(mnSerde.micronaut.serde.jackson)
     implementation(platform(mnTest.boms.testcontainers))
     implementation(libs.testcontainers.junit.jupiter)
